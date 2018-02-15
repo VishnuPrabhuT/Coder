f5c87c8d27b075f8304cebae9d64dd47769d3f08
Vishnu Prabhu Thirugnanasambandam <vthirug@clemson.edu>
2018-02-15 01:05:38 -0500

    Update Symmetric Tree.py
    New Version!

--- Symmetric Tree.py
+++ Symmetric Tree.py
@@ -25,3 +25,29 @@ class Solution(object):
         else:
             return False
         
+########################################################################################################################################
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if root:
+            return True and self.isMirror(root.left, root.right)
+        else:
+            return True
+    def isMirror(self, left, right):
+        if left is None and right is None:
+            return True
+        if left is None or right is None:
+            return False
+        if left and right:
+            print(left.val, right.val)
+            return left.val == right.val and self.isMirror(left.left,right.right) and self.isMirror(left.right,right.left)
