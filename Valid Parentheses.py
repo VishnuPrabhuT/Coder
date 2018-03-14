cd6568ce93d4aad61edb769d68ee929be26ef218
Vishnu Prabhu Thirugnanasambandam <vthirug@clemson.edu>
2018-03-14 15:34:14 -0400

    Update Valid Parentheses.py

--- Valid Parentheses.py
+++ Valid Parentheses.py
@@ -1,3 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        def checkSym(left, right):
+            if not left and not right:
+                return True
+            elif not left or not right:
+                return False
+            if left.val == right.val:
+                l = checkSym(left.left, right.right)
+                r = checkSym(left.right, right.left)
+            else:
+                return False
+            return l and r
+        return checkSym(root.left, root.right)
+
+#########################################################################
 class Solution:
     def isValid(self, s):
         """
