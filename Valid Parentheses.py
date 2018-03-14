0ca1550b2bef8ba978e32492543b8b3bd31f27dc
Vishnu Prabhu Thirugnanasambandam <vthirug@clemson.edu>
2018-03-14 14:31:11 -0400

    Update Valid Parentheses.py

--- Valid Parentheses.py
+++ Valid Parentheses.py
@@ -1,3 +1,24 @@
+class Solution:
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        openB = {'{': '}', '[': ']', '(': ')'}
+        closeB = {'}': '{', ']': '[', ')': '('}
+        stack = []
+        count = 0
+        for index, c in enumerate(s):
+            if c in openB.keys():
+                stack.append(c)
+            elif not stack or openB[stack.pop()] != c:
+                return False
+            count += (c in openB.keys()) - (c in openB.values())
+        return count == 0
+
+
+
+##########################################################################
 class Solution(object):
     def isValid(self, s):
         """
