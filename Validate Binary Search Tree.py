e37ad939ed4bd20a177ecdaf544028f53af049a1
Vishnu Prabhu Thirugnanasambandam <vthirug@clemson.edu>
2018-04-05 15:07:42 -0400

    Update Validate Binary Search Tree.py

--- Validate Binary Search Tree.py
+++ Validate Binary Search Tree.py
@@ -11,6 +11,25 @@ class Solution(object):
         :type root: TreeNode
         :rtype: bool
         """
+        self.pre = None
+        def isBST(root):
+            if not root:
+                return True
+            if not isBST(root.left):
+                return False
+            if self.pre and self.pre.val >= root.val:
+                return False
+            self.pre = root
+            return isBST(root.right)
+        return isBST(root)
+
+#####################################################################################################
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
         def isBST(root, left, right):
             if not root:
                 return True
