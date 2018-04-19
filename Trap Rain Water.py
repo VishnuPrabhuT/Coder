0661497a40c7bf9f483b5208cc3e3e25707f1e67
Vishnu Prabhu Thirugnanasambandam <vthirug@clemson.edu>
2018-04-19 18:31:05 -0400

    Update Trap Rain Water.py

--- Trap Rain Water.py
+++ Trap Rain Water.py
@@ -17,3 +17,23 @@ class Solution(object):
                 rMax = max(height[right], rMax)
                 right -= 1
         return res
+    
+ ############################  Stack  #######################################
+
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        stacky = []
+        i, res = 0, 0
+        while i < len(height):
+            if not stacky or height[stacky[-1]] >= height[i]:
+                stacky.append(i)
+                i += 1
+            else:
+                top = stacky.pop()
+                trap = 0 if not stacky else (min(height[i], height[stacky[-1]]) - height[top]) * (i-stacky[-1] - 1)
+                res += trap
+        return res
